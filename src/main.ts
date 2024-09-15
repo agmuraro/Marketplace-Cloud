b58be39cabeb172457ce2e5da4afe4e2ec19d1f3
agmuraro <arthurmuraro18@gmail.com>
2024-09-15 14:27:45 -0300

    swagger pronto

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('API de exemplo')
-    .setDescription('Documentação da API de exemplo')
+    .setTitle('Marketplace')
     .setVersion('1.0')
     .build();
   
