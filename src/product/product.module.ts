d724eef9cf5469e225157b98ad005d33bc33cf10
agmuraro <arthurmuraro18@gmail.com>
2024-10-30 15:28:52 -0300

    ajustes

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -3,9 +3,12 @@ import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 import { ProductRepository } from './product.repository';
 import { PrismaService } from 'src/prisma.service';
+import { ServiceBusService } from './serviceBus';
 
 @Module({
+  imports: [ServiceBusService],
   controllers: [ProductController],
-  providers: [ProductService, ProductRepository, PrismaService]
+  providers: [ProductService, ProductRepository, PrismaService, ServiceBusService]
 })
 export class ProductModule {}
+
