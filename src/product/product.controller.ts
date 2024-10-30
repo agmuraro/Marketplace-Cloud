8113e4585b5aa641322d7823a9f4155dbe48b556
agmuraro <arthurmuraro18@gmail.com>
2024-10-30 15:04:26 -0300

    service bus + microserviço

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,34 +1,58 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { ServiceBusService } from './serviceBus'; 
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
 @ApiTags('product')
 @Controller('product')
 export class ProductController {
-    constructor(private service: ProductService){}
+   constructor(
+       private service: ProductService,
+       private serviceBusService: ServiceBusService 
+   ) {}
 
-    @Post()
-    async createProduct(@Body() body: any){
-        return await this.service.createProduct(body)
-    }
+   @Post()
+   async createProduct(@Body() body: any) {
+       const createdProduct = await this.service.createProduct(body);
+       
+       await this.serviceBusService.sendMessage('Produto criado com sucesso!');
+       
+       return createdProduct;
+   }
 
-    @Get('findMany')
-    async findProducts(){
-        return await this.service.findProducts()
-    }
+   @Get('findMany')
+   async findProducts() {
+       const products = await this.service.findProducts();
+       
+       await this.serviceBusService.sendMessage('Produtos resgatados');
+       
+       return products;
+   }
 
-    @Get(':id')
-    async findProduct(@Param('id') id: string){
-        return await this.service.findProduct(id)
-    }
+   @Get(':id')
+   async findProduct(@Param('id') id: string) {
+       const product = await this.service.findProduct(id);
+       
+       await this.serviceBusService.sendMessage(`Produto resgatado: ${id}`);
+       
+       return product;
+   }
 
-    @Put()
-    async updateProduct(@Body()body: any){
-        return await this.service.updateProduct(body)
-    }
+   @Put()
+   async updateProduct(@Body() body: any) {
+       const updatedProduct = await this.service.updateProduct(body);
+       
+       await this.serviceBusService.sendMessage('Produto atualizado');
+       
+       return updatedProduct;
+   }
 
-    @Delete()
-    async deleteProduct(@Body()body: any){
-        return await this.service.deleteProduct(body)
-    }
+   @Delete()
+   async deleteProduct(@Body() body: any) {
+       const deletedProduct = await this.service.deleteProduct(body);
+       
+       await this.serviceBusService.sendMessage('Produto excluído');
+       
+       return deletedProduct;
+   }
 }
