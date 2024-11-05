9ae38c80a16432fec62068f2c5d81e35f6858f5e
agmuraro <arthurmuraro18@gmail.com>
2024-11-05 15:19:37 -0300

    service bus ok

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { ServiceBusService } from './serviceBus'; 
+import { ServiceBusService } from './serviceBus';
 import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
 @ApiTags('product')
@@ -14,45 +14,35 @@ export class ProductController {
    @Post()
    async createProduct(@Body() body: any) {
        const createdProduct = await this.service.createProduct(body);
-       
-       await this.serviceBusService.sendMessage('Produto criado com sucesso!');
-       
+       await this.serviceBusService.sendMessage('create', { id: createdProduct.id });
        return createdProduct;
    }
 
    @Get('findMany')
    async findProducts() {
        const products = await this.service.findProducts();
-       
-       await this.serviceBusService.sendMessage('Produtos resgatados');
-       
+       await this.serviceBusService.sendMessage('findMany', {});
        return products;
    }
 
    @Get(':id')
    async findProduct(@Param('id') id: string) {
        const product = await this.service.findProduct(id);
-       
-       await this.serviceBusService.sendMessage(`Produto resgatado: ${id}`);
-       
+       await this.serviceBusService.sendMessage('findOne', { id });
        return product;
    }
 
    @Put()
    async updateProduct(@Body() body: any) {
        const updatedProduct = await this.service.updateProduct(body);
-       
-       await this.serviceBusService.sendMessage('Produto atualizado');
-       
+       await this.serviceBusService.sendMessage('update', { id: updatedProduct.id });
        return updatedProduct;
    }
 
-   @Delete()
-   async deleteProduct(@Body() body: any) {
-       const deletedProduct = await this.service.deleteProduct(body);
-       
-       await this.serviceBusService.sendMessage('Produto excluído');
-       
-       return deletedProduct;
+   @Delete( )
+   async deleteProduct(@Body() body: {id:string}) {
+       await this.service.deleteProduct(body);
+       await this.serviceBusService.sendMessage('delete', { body });
+       return { message: 'Produto excluído com sucesso' };
    }
 }
--- src/product/serviceBus.ts
+++ src/product/serviceBus.ts
@@ -1,22 +1,51 @@
-import { ServiceBusClient } from "@azure/service-bus";
+import { ServiceBusClient, ServiceBusSender } from "@azure/service-bus";
 
 export class ServiceBusService {
     private client: ServiceBusClient;
-    private sender;
+    private senders: { [key: string]: ServiceBusSender };
 
     constructor() {
         const connectionString = process.env.AZURE_SERVICE_BUS_KEY;
         this.client = new ServiceBusClient(connectionString);
-        this.sender = this.client.createSender("filaproduto");
+
+        // Inicialize senders para cada fila
+        this.senders = {
+            filaproduto: this.client.createSender("filaproduto"),
+            filaget: this.client.createSender("filaget"),
+            filaput: this.client.createSender("filaput"),
+            filadelete: this.client.createSender("filadelete"),
+        };
     }
 
-    async sendMessage(message: string) {
-        await this.sender.sendMessages({ body: message });
-        console.log("Mensagem enviada para o Service Bus");
+    private getQueueNameByAction(action: string): string {
+        switch (action.toLowerCase()) {
+            case "create":
+                return "filaproduto";
+            case "findmany":
+                return "filaget";
+            case "update":
+                return "filaput";
+            case "delete":
+                return "filadelete";
+            default:
+                throw new Error(`Ação ${action} não suportada.`);
+        }
     }
-    
+
+    async sendMessage(action: string, data: any) {
+        const queueName = this.getQueueNameByAction(action);
+        const sender = this.senders[queueName];
+
+        await sender.sendMessages({
+            body: { action, ...data }
+        });
+        console.log(`Mensagem enviada para o Service Bus na fila ${queueName} com ação: ${action}`);
+    }
+
     async close() {
-        await this.sender.close();
+        for (const sender of Object.values(this.senders)) {
+            await sender.close();
+        }
         await this.client.close();
     }
 }
